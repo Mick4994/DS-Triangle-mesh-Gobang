889931c6658081a2d5ef5d48bd9ae92a7857a50f
zhaozhong1 <104846639+zhaozhong1@users.noreply.github.com>
2023-06-19 18:48:40 +0800

    AI下棋头文件

---
+++ include/AIChess.h
@@ -0,0 +1,10 @@
+//
+// Created by 22843 on 2023/6/19.
+//
+#include "../include/ChessBoardInit.h"
+#include "../include/ChessBoardUI.h"
+
+#ifndef EASYX_AICHESS_H
+#define EASYX_AICHESS_H
+void aiMakeMove(ChessBoard &board);
+#endif //EASYX_AICHESS_H
